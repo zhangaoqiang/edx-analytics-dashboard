8dadc652c625f100785d7c0181e9a3ad60ccc9b2
Kyle McCormick <kmccormick@edx.org>
2017-06-12 15:44:22 -0400

    Improve performance of CourseSummariesPresenter
    CourseSummariesPresenter.get_course_summaries now passes required
    course IDs to the course_summaries API call, reducing the size of the
    returned list.
    
    EDUCATOR-464

--- analytics_dashboard/courses/presenters/course_summaries.py
+++ analytics_dashboard/courses/presenters/course_summaries.py
@@ -19,23 +19,6 @@ class CourseSummariesPresenter(BasePresenter):
             return all_summaries
         return [summary for summary in all_summaries if summary['course_id'] in course_ids]
 
-    def _get_all_summaries(self):
-        """
-        Returns all course summaries. If not cached, summaries will be fetched
-        from the analytics data API.
-        """
-        all_summaries = cache.get(self.CACHE_KEY)
-        if all_summaries is None:
-            exclude = ['programs']  # we make a separate call to the programs endpoint
-            if not switch_is_active('enable_course_passing'):
-                exclude.append('passing_users')
-            all_summaries = self.client.course_summaries().course_summaries(exclude=exclude)
-            all_summaries = [
-                {field: ('' if val is None and field in self.NON_NULL_STRING_FIELDS else val)
-                 for field, val in summary.items()} for summary in all_summaries]
-            cache.set(self.CACHE_KEY, all_summaries, settings.COURSE_SUMMARIES_CACHE_TIMEOUT)
-        return all_summaries
-
     def _get_last_updated(self, summaries):
         # all the create times should be the same, so just use the first one
         if summaries:
@@ -48,15 +31,27 @@ class CourseSummariesPresenter(BasePresenter):
         Returns course summaries that match those listed in course_ids.  If
         no course IDs provided, all data will be returned.
         """
-        all_summaries = self._get_all_summaries()
-        filtered_summaries = self.filter_summaries(all_summaries, course_ids)
+        exclude = ['programs']  # we make a separate call to the programs endpoint
+        if not switch_is_active('enable_course_passing'):
+            exclude.append('passing_users')
+        summaries = self.client.course_summaries().course_summaries(course_ids=course_ids, exclude=exclude)
+        summaries = [
+            {
+                field: (
+                    '' if val is None and field in self.NON_NULL_STRING_FIELDS
+                    else val
+                )
+                for field, val in summary.items()
+            } for summary in summaries
+        ]
 
         # sort by title by default with "None" values at the end
-        filtered_summaries = sorted(
-            filtered_summaries,
-            key=lambda x: (not x['catalog_course_title'], x['catalog_course_title']))
+        summaries = sorted(
+            summaries,
+            key=lambda x: (not x['catalog_course_title'], x['catalog_course_title'])
+        )
 
-        return filtered_summaries, self._get_last_updated(filtered_summaries)
+        return summaries, self._get_last_updated(summaries)
 
     def get_course_summary_metrics(self, summaries):
         summary = {
